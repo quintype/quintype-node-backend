9db891b043b0ebd12e11bdaa54f0a96c676f8f0b
Sinu John <sinujohn91@gmail.com>
2019-05-13 18:10:30 +0530

    feat(add-access-fields): add access and access-level-value in load nested collections

--- constants.js
+++ constants.js
@@ -2,6 +2,6 @@ const DEFAULT_LIMIT = '10';
 
 const DEFAULT_DEPTH = 1;
 
-const DEFAULT_STORY_FIELDS = "headline,subheadline,slug,url,hero-image-s3-key,hero-image-caption,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,authors,author-name,author-id,sections,story-template,metadata";
+const DEFAULT_STORY_FIELDS = "headline,subheadline,slug,url,hero-image-s3-key,hero-image-caption,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,authors,author-name,author-id,sections,story-template,metadata,access,access-level-value";
 
 module.exports = {DEFAULT_DEPTH, DEFAULT_LIMIT, DEFAULT_STORY_FIELDS};
