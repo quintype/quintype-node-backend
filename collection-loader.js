a3e4aeec65ddcff522a5fe46feb1cdce667ef165
Tejas Dinkar <tejas@gja.in>
2019-12-18 17:11:41 +0530

    feat: Adding an option for storyLimits on collection by slug, which limits the number of stories

--- collection-loader.js
+++ collection-loader.js
@@ -2,13 +2,15 @@ const get = require("lodash/get");
 const flatMap = require("lodash/flatMap");
 const {DEFAULT_STORY_FIELDS} = require("./constants");
 
-function loadCollectionItems(client, collections, storyFields) {
+function loadCollectionItems(client, collections, {storyFields, storyLimits}) {
   const bulkRequestBody = collections.reduce(
     (acc, collection) => Object.assign(acc, {
       [collection.slug]: {
         _type: "collection",
         slug: collection.slug,
-        "story-fields": storyFields}
+        "story-fields": storyFields,
+        limit: storyLimits[get(collection, ["associated-metadata", "layout"])]
+      }
     }), {});
 
   return client.getInBulk({requests: bulkRequestBody}).then(response => response.results);
@@ -16,24 +18,24 @@ function loadCollectionItems(client, collections, storyFields) {
 
 // Ugly. This function updates all the items in place.
 // However, this is way more readable than a pure version
-function updateItemsInPlace(client, depth, items, storyFields) {
+function updateItemsInPlace(client, depth, items, {storyFields, storyLimits}) {
   const collections = items.filter(item => item && item.type == "collection");
 
   if(depth == 0 || collections.length == 0)
     return Promise.resolve();
 
-  return loadCollectionItems(client, collections, storyFields)
+  return loadCollectionItems(client, collections, {storyFields, storyLimits})
     .then(collectionSlugToCollection => {
       collections.forEach(collection => {
         collection.summary = get(collectionSlugToCollection, [collection.slug, "summary"], '')
         collection.items = get(collectionSlugToCollection, [collection.slug, "items"])
       });
-      return updateItemsInPlace(client, depth - 1, flatMap(collections, collection => collection.items), storyFields)
+      return updateItemsInPlace(client, depth - 1, flatMap(collections, collection => collection.items), {storyFields, storyLimits})
     })
 }
 
-function loadNestedCollectionData(client, collection, {depth, storyFields}) {
-  return updateItemsInPlace(client, depth, collection.items, storyFields)
+function loadNestedCollectionData(client, collection, {depth, storyFields, storyLimits}) {
+  return updateItemsInPlace(client, depth, collection.items, {storyFields, storyLimits})
     .then(() => collection);
 }
 
