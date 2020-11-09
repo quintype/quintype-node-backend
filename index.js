42febb42af405fa3f8d229940c3752a85e3ee7a8
nandap4790 <nandap4790@gmail.com>
2020-11-09 18:47:44 +0530

    update package

--- index.js
+++ index.js
@@ -288,6 +288,7 @@ class Collection extends BaseAPI {
    * @see {@link https://developers.quintype.com/swagger/#/collection/get_api_v1_collections__slug_ GET /api/v1/collections/:slug} API documentation for a list of parameters and fields
    */
   static getCollectionBySlug(client, slug, params, options = {}) {
+    console.log('inside this now----');
     const {depth = DEFAULT_DEPTH, storyLimits = {}} = options;
     const storyFields = _.get(params, ["story-fields"], DEFAULT_STORY_FIELDS);
 
