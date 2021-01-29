fd47f0f20e281fcbeeb43942ec70fb147323239b
Bindiya <bindiya@quintype.com>
2021-01-29 09:58:29 +0530

    Add read-time in default story fields

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-const DEFAULT_LIMIT = '10';
+const DEFAULT_LIMIT = "10";
 
 const DEFAULT_DEPTH = 1;
 
@@ -6,6 +6,7 @@ const DEFAULT_DEPTH = 1;
  * @package
  * Default Fields for Most APIs which get stories in bulk
  */
-const DEFAULT_STORY_FIELDS = "headline,subheadline,slug,url,hero-image-s3-key,hero-image-caption,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,authors,author-name,author-id,sections,story-template,metadata,access,access-level-value";
+const DEFAULT_STORY_FIELDS =
+  "headline,subheadline,slug,url,hero-image-s3-key,hero-image-caption,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,authors,author-name,author-id,sections,story-template,metadata,access,access-level-value,read-time";
 
-module.exports = {DEFAULT_DEPTH, DEFAULT_LIMIT, DEFAULT_STORY_FIELDS};
+module.exports = { DEFAULT_DEPTH, DEFAULT_LIMIT, DEFAULT_STORY_FIELDS };
