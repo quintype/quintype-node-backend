333f95764c9ce926bd18647b5c3925881773fd55
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-04-16 10:28:53 +0530

    WIP

--- collection-loader.js
+++ collection-loader.js
@@ -1,42 +1,81 @@
-const get = require("lodash/get");
-const flatMap = require("lodash/flatMap");
-const {DEFAULT_STORY_FIELDS} = require("./constants");
+const get = require('lodash/get');
+const flatMap = require('lodash/flatMap');
+const {DEFAULT_STORY_FIELDS} = require('./constants');
 
 function loadCollectionItems(client, collections, {storyFields, storyLimits}) {
   const bulkRequestBody = collections.reduce(
-    (acc, collection) => Object.assign(acc, {
-      [collection.slug]: {
-        _type: "collection",
-        slug: collection.slug,
-        "story-fields": storyFields,
-        limit: storyLimits[get(collection, ["associated-metadata", "layout"])]
-      }
-    }), {});
+    (acc, collection) =>
+      Object.assign(acc, {
+        [collection.slug]: {
+          _type: 'collection',
+          slug: collection.slug,
+          'story-fields': storyFields,
+          limit:
+            storyLimits[get(collection, ['associated-metadata', 'layout'])],
+        },
+      }),
+    {}
+  );
 
-  return client.getInBulk({requests: bulkRequestBody}).then(response => response.results);
+  return client
+    .getInBulk({requests: bulkRequestBody})
+    .then((response) => response.results);
 }
 
 // Ugly. This function updates all the items in place.
 // However, this is way more readable than a pure version
-function updateItemsInPlace(client, depth, items, {storyFields, storyLimits}) {
-  const collections = items.filter(item => item && item.type == "collection");
+function updateItemsInPlace(
+  client,
+  depth,
+  items,
+  {storyFields, storyLimits},
+  itemsDepth
+) {
+  const collections = items.filter((item) => item && item.type == 'collection');
 
-  if(depth == 0 || collections.length == 0)
-    return Promise.resolve();
+  if (depth == 0 || collections.length == 0) return Promise.resolve();
 
-  return loadCollectionItems(client, collections, {storyFields, storyLimits})
-    .then(collectionSlugToCollection => {
-      collections.forEach(collection => {
-        collection.summary = get(collectionSlugToCollection, [collection.slug, "summary"], '')
-        collection.items = get(collectionSlugToCollection, [collection.slug, "items"])
-      });
-      return updateItemsInPlace(client, depth - 1, flatMap(collections, collection => collection.items), {storyFields, storyLimits})
-    })
+  console.log('fooo1111---------', depth);
+  console.log('fooo2222---------', itemsDepth);
+  console.log('fooo3333---------', itemsDepth === 2 && depth === 1);
+
+  return loadCollectionItems(client, collections, {
+    storyFields,
+    storyLimits,
+  }).then((collectionSlugToCollection) => {
+    collections.forEach((collection) => {
+      collection.summary = get(
+        collectionSlugToCollection,
+        [collection.slug, 'summary'],
+        ''
+      );
+      collection.items = get(collectionSlugToCollection, [
+        collection.slug,
+        'items',
+      ]);
+    });
+    return updateItemsInPlace(
+      client,
+      depth - 1,
+      flatMap(collections, (collection) => collection.items),
+      {storyFields, storyLimits},
+      itemsDepth
+    );
+  });
 }
 
-function loadNestedCollectionData(client, collection, {depth, storyFields, storyLimits}) {
-  return updateItemsInPlace(client, depth, collection.items, {storyFields, storyLimits})
-    .then(() => collection);
+function loadNestedCollectionData(
+  client,
+  collection,
+  {depth, storyFields, storyLimits}
+) {
+  return updateItemsInPlace(
+    client,
+    depth,
+    collection.items,
+    {storyFields, storyLimits},
+    depth
+  ).then(() => collection);
 }
 
 module.exports = {loadNestedCollectionData};
