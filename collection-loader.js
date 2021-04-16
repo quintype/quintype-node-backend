abd12884aab8598f60fb2076c364590bad8a1143
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-04-16 12:29:28 +0530

    WIP

--- collection-loader.js
+++ collection-loader.js
@@ -24,19 +24,10 @@ function loadCollectionItems(client, collections, {storyFields, storyLimits}) {
 
 // Ugly. This function updates all the items in place.
 // However, this is way more readable than a pure version
-function updateItemsInPlace(
-  client,
-  depth,
-  items,
-  {storyFields, storyLimits, itemsDepth}
-) {
+function updateItemsInPlace(client, depth, items, {storyFields, storyLimits}) {
   const collections = items.filter((item) => item && item.type == 'collection');
 
   if (depth == 0 || collections.length == 0) return Promise.resolve();
-
-  console.log('fooo1111---------', depth);
-  console.log('fooo2222---------', itemsDepth);
-  console.log('fooo3333---------', itemsDepth === 2 && depth === 1);
 
   return loadCollectionItems(client, collections, {
     storyFields,
@@ -57,7 +48,7 @@ function updateItemsInPlace(
       client,
       depth - 1,
       flatMap(collections, (collection) => collection.items),
-      {storyFields, storyLimits, itemsDepth}
+      {storyFields, storyLimits}
     );
   });
 }
@@ -70,7 +61,6 @@ function loadNestedCollectionData(
   return updateItemsInPlace(client, depth, collection.items, {
     storyFields,
     storyLimits,
-    itemsDepth: 2,
   }).then(() => collection);
 }
 
