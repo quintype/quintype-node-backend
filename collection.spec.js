bf6ec530a9a68bcb59a632d57520c08a578ce5ae
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-04-30 13:06:07 +0530

    add test when depth is 2 and default limit is 4

--- collection.spec.js
+++ collection.spec.js
@@ -28,39 +28,81 @@ function getClient({
 
   getInBulk = (request) => {
     let items = [];
-    for (let j = 0; j < request.requests['arrow-collection'].limit; j++) {
-      items.push({
-        id: 109047,
-        'associated-metadata': {},
-        type: 'collection',
-        name: 'Football (Sports)',
-        slug: 'football-sports',
-        template: 'section',
-        metadata: {section: [Array]},
-        'collection-date': null,
-      });
+    if(request.requests['arrow-collection']) {
+        for (let j = 0; j < request.requests['arrow-collection'].limit; j++) {
+            items.push({
+              id: 109047,
+              'associated-metadata': {},
+              type: 'collection',
+              name: 'Football (Sports)',
+              slug: 'football-sports',
+              template: 'section',
+              metadata: {section: [Array]},
+              'collection-date': null,
+            });
+          }
     }
 
-    const collectionItem = {
-      'arrow-collection': {
-        'updated-at': 1619698289776,
-        'collection-cache-keys': ['c/1363/146801'],
-        slug: 'arrow-collection',
-        fallback: false,
-        name: 'Arrow collection',
-        'data-source': 'manual',
-        automated: false,
-        template: 'default',
-        rules: {},
-        summary: null,
-        id: 146801,
-        'total-count': 10,
-        'collection-date': null,
-        items: items,
-        'created-at': 1618817988238,
-        metadata: {'cover-image': null},
-      },
-    };
+    if(request.requests['football-sports']) {
+        for (let j = 0; j < request.requests['football-sports'].limit; j++) {
+            items.push({
+              id: 109047,
+              'associated-metadata': {},
+              type: 'collection',
+              name: 'hello india',
+              slug: 'hello',
+              template: 'section',
+              metadata: {section: [Array]},
+              'collection-date': null,
+            });
+          }
+    }
+
+    // console.log("items----", items)
+
+    let collectionItem =  {
+        'football-sports': {
+          'updated-at': 1619698289776,
+          'collection-cache-keys': ['c/1363/146801'],
+          slug: 'football-sports',
+          fallback: false,
+          name: 'Arrow collection',
+          'data-source': 'manual',
+          automated: false,
+          template: 'default',
+          rules: {},
+          summary: null,
+          id: 146801,
+          'total-count': 10,
+          'collection-date': null,
+          items: items,
+          'created-at': 1618817988238,
+          metadata: {'cover-image': null},
+        },
+      };
+
+    if(request.requests['arrow-collection']) {
+        collectionItem = {
+            'arrow-collection': {
+              'updated-at': 1619698289776,
+              'collection-cache-keys': ['c/1363/146801'],
+              slug: 'arrow-collection',
+              fallback: false,
+              name: 'Arrow collection',
+              'data-source': 'manual',
+              automated: false,
+              template: 'default',
+              rules: {},
+              summary: null,
+              id: 146801,
+              'total-count': 10,
+              'collection-date': null,
+              items: items,
+              'created-at': 1618817988238,
+              metadata: {'cover-image': null},
+            },
+          };
+    }
 
     return Promise.resolve({
       results: collectionItem,
@@ -76,7 +118,7 @@ function getClient({
 
 describe('Collection', function () {
   describe('Returns Home Collection based on defaultNestedLimit', function () {
-    it('Returns home-collection with depth of 1', async function () {
+    it('Returns home-collection with depth of 1 and defaultNestedLimit of 3', async function () {
       const homeCollectionData = await Collection.getCollectionBySlug(
         getClient(),
         'home',
@@ -85,5 +127,15 @@ describe('Collection', function () {
       );
       expect(homeCollectionData.collection.items[0].items.length).toBe(3);
     });
+    it('Returns home-collection with depth of 2 and defaultNestedLimit of 3', async function () {
+        const homeCollectionData = await Collection.getCollectionBySlug(
+          getClient(),
+          'home',
+          {},
+          {depth: 2, defaultNestedLimit: 4}
+        );
+        console.log("homeCollectionData-------",homeCollectionData.collection.items[0].items)
+        expect(homeCollectionData.collection.items[0].items[0].items.length).toBe(4);
+      });
   });
 });
