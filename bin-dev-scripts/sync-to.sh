f44b4cff53e6a01f6aa065f843950488ab349222
Prabu K <prabu.karuppiah@quintype.com>
2021-05-12 18:56:52 +0530

    update file location for front-end apps and retrigger build

--- bin-dev-scripts/sync-to.sh
+++ bin-dev-scripts/sync-to.sh
@@ -9,4 +9,5 @@ fi
 
 rsync -r  --exclude=node_modules\
   . \
-  "$PATH_TO_APP/node_modules/@quintype/backend/"
+  "$PATH_TO_APP/node_modules/@quintype/framework/node_modules/@quintype/backend/"\
+  && touch "$PATH_TO_APP/app/isomorphic/pick-component.js"
