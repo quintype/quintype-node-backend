60a866235ea379503ab379ea4cde6a79c32d2524
Harshith Raj <harshithraj@Harshiths-MacBook-Pro.local>
2021-08-24 15:27:05 +0530

    update test

--- collection.spec.js
+++ collection.spec.js
@@ -1,143 +1,152 @@
-const {Collection} = require('./index');
+const { Collection } = require("./index");
+const _ = require("lodash");
 
 function getClient({
-  getCollectionBySlug = (slug) => {
+  getCollectionBySlug = slug => {
     return Promise.resolve({
-      slug: 'home',
+      slug: "home",
       summary: null,
       items: [
         {
-          'associated-metadata': {layout: 'ArrowFourColTwelveStories'},
-          type: 'collection',
-          slug: 'arrow-collection',
-        },
-      ],
+          "associated-metadata": { layout: "ArrowFourColTwelveStories" },
+          type: "collection",
+          slug: "arrow-collection"
+        }
+      ]
     });
   },
 
-  getInBulk = (request) => {
+  getInBulk = request => {
     let collectionItem = {};
     const requestKeys = Object.keys(request.requests);
 
     if (requestKeys.length > 0) {
-      requestKeys.map((req) => {
+      requestKeys.map(req => {
         let items = [];
         const limit = request.requests[req].limit || 40;
 
         for (let i = 0; i < limit; i++) {
           items.push({
-            'associated-metadata': {},
-            type: 'collection',
-            slug: `football-sports-${i}`,
+            "associated-metadata": {},
+            type: "collection",
+            slug: `football-sports-${i}`
           });
         }
 
         Object.assign(collectionItem, {
           [req]: {
             slug: req,
-            items: items,
-          },
+            items: items
+          }
         });
       });
     }
 
     return Promise.resolve({
-      results: collectionItem,
+      results: collectionItem
     });
-  },
+  }
 } = {}) {
   const clientObj = {
     getCollectionBySlug,
-    getInBulk,
+    getInBulk
   };
   return clientObj;
 }
 
-describe('Collection', function () {
-  describe('Returns Home Collection based on defaultNestedLimit', function () {
-    it('Returns home-collection with depth of 1 and defaultNestedLimit of 3', async function () {
+function request(slug, params) {
+  throw { response: { status: 404 } };
+}
+
+function catch404(e, defaultValue) {
+  const statusCode = _.get(e, ["response", "status"]);
+  if (statusCode === 404) return Promise.resolve(defaultValue);
+  throw e;
+}
+
+function getClientWhenCollectionDoesNotExist({
+  getCollectionBySlug = (slug, params) => {
+    try {
+      return request("/api/v1/collections/" + slug, {
+        qs: params
+      });
+    } catch (e) {
+      return catch404(e, null);
+    }
+  }
+} = {}) {
+  return { getCollectionBySlug };
+}
+
+describe("Collection", function() {
+  it("foo", async function() {
+    const homeCollectionData = await Collection.getCollectionBySlug(
+      getClientWhenCollectionDoesNotExist(),
+      "hot-news",
+      {},
+      { depth: 1, defaultNestedLimit: 3 }
+    );
+    expect(homeCollectionData).toBe(null);
+  });
+  describe("Returns Home Collection based on defaultNestedLimit", function() {
+    it("Returns home-collection with depth of 1 and defaultNestedLimit of 3", async function() {
       const homeCollectionData = await Collection.getCollectionBySlug(
         getClient(),
-        'home',
+        "home",
         {},
-        {depth: 1, defaultNestedLimit: 3}
+        { depth: 1, defaultNestedLimit: 3 }
       );
       expect(homeCollectionData.collection.items[0].items.length).toBe(3);
     });
-    it('Returns home-collection with depth of 2 and defaultNestedLimit of 3', async function () {
+    it("Returns home-collection with depth of 2 and defaultNestedLimit of 3", async function() {
       const homeCollectionData = await Collection.getCollectionBySlug(
         getClient(),
-        'home',
+        "home",
         {},
-        {depth: 2, defaultNestedLimit: 4}
+        { depth: 2, defaultNestedLimit: 4 }
       );
       expect(homeCollectionData.collection.items[0].items.length).toBe(4);
-      expect(homeCollectionData.collection.items[0].items[0].items.length).toBe(
-        4
-      );
+      expect(homeCollectionData.collection.items[0].items[0].items.length).toBe(4);
     });
-    it('Returns home-collection with depth of 2', async function () {
-      const homeCollectionData = await Collection.getCollectionBySlug(
-        getClient(),
-        'home',
-        {},
-        {depth: 2}
-      );
-      expect(homeCollectionData.collection.items[0].items[0].items.length).toBe(
-        40
-      );
+    it("Returns home-collection with depth of 2", async function() {
+      const homeCollectionData = await Collection.getCollectionBySlug(getClient(), "home", {}, { depth: 2 });
+      expect(homeCollectionData.collection.items[0].items[0].items.length).toBe(40);
     });
   });
-  describe('Returns Home Collection based on nestedCollectionLimit', function () {
-    it('Returns home-collection with depth of 2 ', async function () {
+  describe("Returns Home Collection based on nestedCollectionLimit", function() {
+    it("Returns home-collection with depth of 2 ", async function() {
       const homeCollectionData = await Collection.getCollectionBySlug(
         getClient(),
-        'home',
+        "home",
         {},
         {
           depth: 2,
           defaultNestedLimit: 4,
-          nestedCollectionLimit: {ArrowFourColTwelveStories: [1, 2, 3, 5]},
+          nestedCollectionLimit: { ArrowFourColTwelveStories: [1, 2, 3, 5] }
         }
       );
       expect(homeCollectionData.collection.items[0].items.length).toBe(4);
-      expect(homeCollectionData.collection.items[0].items[0].items.length).toBe(
-        1
-      );
-      expect(homeCollectionData.collection.items[0].items[1].items.length).toBe(
-        2
-      );
-      expect(homeCollectionData.collection.items[0].items[2].items.length).toBe(
-        3
-      );
-      expect(homeCollectionData.collection.items[0].items[3].items.length).toBe(
-        5
-      );
+      expect(homeCollectionData.collection.items[0].items[0].items.length).toBe(1);
+      expect(homeCollectionData.collection.items[0].items[1].items.length).toBe(2);
+      expect(homeCollectionData.collection.items[0].items[2].items.length).toBe(3);
+      expect(homeCollectionData.collection.items[0].items[3].items.length).toBe(5);
     });
-    it('Returns home-collection with depth of 2 and defaultlimit for the last nested collection ', async function () {
+    it("Returns home-collection with depth of 2 and defaultlimit for the last nested collection ", async function() {
       const homeCollectionData = await Collection.getCollectionBySlug(
         getClient(),
-        'home',
+        "home",
         {},
         {
           depth: 2,
           defaultNestedLimit: 4,
-          nestedCollectionLimit: {ArrowFourColTwelveStories: [6, 7, 8]},
+          nestedCollectionLimit: { ArrowFourColTwelveStories: [6, 7, 8] }
         }
       );
       expect(homeCollectionData.collection.items[0].items.length).toBe(4);
-      expect(homeCollectionData.collection.items[0].items[0].items.length).toBe(
-        6
-      );
-      expect(homeCollectionData.collection.items[0].items[1].items.length).toBe(
-        7
-      );
-      expect(homeCollectionData.collection.items[0].items[2].items.length).toBe(
-        8
-      );
-      expect(homeCollectionData.collection.items[0].items[3].items.length).toBe(
-        4
-      );
+      expect(homeCollectionData.collection.items[0].items[0].items.length).toBe(6);
+      expect(homeCollectionData.collection.items[0].items[1].items.length).toBe(7);
+      expect(homeCollectionData.collection.items[0].items[2].items.length).toBe(8);
+      expect(homeCollectionData.collection.items[0].items[3].items.length).toBe(4);
     });
   });
 });
