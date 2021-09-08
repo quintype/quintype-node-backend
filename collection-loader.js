5caaf7bcf901a5d368fc9ca46916ef0964b430ef
Reena Singh <45616864+ReenaSingh07@users.noreply.github.com>
2021-09-08 10:01:51 +0530

    feat: support collection of collection of stories data based on index (#67)

--- collection-loader.js
+++ collection-loader.js
@@ -1,16 +1,12 @@
-const get = require('lodash/get');
-const flatMap = require('lodash/flatMap');
+const get = require("lodash/get");
+const flatMap = require("lodash/flatMap");
 
-function loadCollectionItems(
-  client,
-  collections,
-  {storyFields, storyLimits, defaultNestedLimit}
-) {
+function loadCollectionItems(client, collections, { storyFields, storyLimits, defaultNestedLimit }) {
   const bulkRequestBody = collections.reduce((acc, collection, index) => {
-    let limit = storyLimits[get(collection, ['associated-metadata', 'layout'])];
+    let limit = storyLimits[get(collection, ["associated-metadata", "layout"])];
 
-    if (!limit && get(collection, ['childCollectionLimit'])) {
-      limit = get(collection, ['childCollectionLimit']);
+    if (!limit && get(collection, ["childCollectionLimit"])) {
+      limit = get(collection, ["childCollectionLimit"]);
     }
 
     if (!limit && defaultNestedLimit) {
@@ -19,17 +15,15 @@ function loadCollectionItems(
 
     return Object.assign(acc, {
       [`${collection.slug}-${index}`]: {
-        _type: 'collection',
+        _type: "collection",
         slug: collection.slug,
-        'story-fields': storyFields,
-        limit: limit,
-      },
+        "story-fields": storyFields,
+        limit: limit
+      }
     });
   }, {});
 
-  return client
-    .getInBulk({requests: bulkRequestBody})
-    .then((response) => response.results);
+  return client.getInBulk({ requests: bulkRequestBody }).then(response => response.results);
 }
 
 // Ugly. This function updates all the items in place.
@@ -38,61 +32,71 @@ function updateItemsInPlace(
   client,
   depth,
   items,
-  {storyFields, storyLimits, defaultNestedLimit, nestedCollectionLimit}
+  { storyFields, storyLimits, defaultNestedLimit, nestedCollectionLimit, collectionOfCollectionsIndexes = [] }
 ) {
-  const collections = items.filter((item) => item && item.type == 'collection');
+  const collections = items.filter(item => item && item.type == "collection");
 
   if (depth == 0 || collections.length == 0) return Promise.resolve();
 
   return loadCollectionItems(client, collections, {
     storyFields,
     storyLimits,
-    defaultNestedLimit,
-  }).then((collectionSlugToCollection) => {
+    defaultNestedLimit
+  }).then(collectionSlugToCollection => {
     collections.forEach((collection, index) => {
       const slugWithIndex = `${collection.slug}-${index}`;
-      collection.summary = get(collectionSlugToCollection,[slugWithIndex, 'summary'], '');
-      collection.automated = get(collectionSlugToCollection, [slugWithIndex, 'automated']);
-      collection['collection-cache-keys'] = get(collectionSlugToCollection, [slugWithIndex, 'collection-cache-keys'], []);
-      collection.items = get(collectionSlugToCollection, [slugWithIndex, 'items'], []);
+      collection.summary = get(collectionSlugToCollection, [slugWithIndex, "summary"], "");
+      collection.automated = get(collectionSlugToCollection, [slugWithIndex, "automated"]);
+      collection["collection-cache-keys"] = get(
+        collectionSlugToCollection,
+        [slugWithIndex, "collection-cache-keys"],
+        []
+      );
+      collection.items = get(collectionSlugToCollection, [slugWithIndex, "items"], []);
 
       if (nestedCollectionLimit && collection.items) {
         collection.items.forEach((item, index) => {
-          if (
-            item.type === 'collection' &&
-            nestedCollectionLimit[
-              get(collection, ['associated-metadata', 'layout'])
-              ]
-          ) {
+          if (item.type === "collection" && nestedCollectionLimit[get(collection, ["associated-metadata", "layout"])]) {
             item.childCollectionLimit =
-              nestedCollectionLimit[
-                get(collection, ['associated-metadata', 'layout'])
-                ][index];
+              nestedCollectionLimit[get(collection, ["associated-metadata", "layout"])][index];
           }
         });
       }
     });
 
-    return updateItemsInPlace(
-      client,
-      depth - 1,
-      flatMap(collections, (collection) => collection.items),
-      {storyFields, storyLimits, defaultNestedLimit}
-    );
+    if (collectionOfCollectionsIndexes.length) {
+      const updatedCollections = collections
+        .filter((_, index) => collectionOfCollectionsIndexes.includes(index))
+        .map(childCollection => {
+          return updateItemsInPlace(client, 1, childCollection.items, {
+            storyFields,
+            storyLimits,
+            defaultNestedLimit
+          });
+        });
+      return Promise.all(updatedCollections).then(collections => collections);
+    } else {
+      return updateItemsInPlace(client, depth - 1, flatMap(collections, collection => collection.items), {
+        storyFields,
+        storyLimits,
+        defaultNestedLimit
+      });
+    }
   });
 }
 
 function loadNestedCollectionData(
   client,
   collection,
-  {depth, storyFields, storyLimits, defaultNestedLimit, nestedCollectionLimit}
+  { depth, storyFields, storyLimits, defaultNestedLimit, nestedCollectionLimit, collectionOfCollectionsIndexes }
 ) {
   return updateItemsInPlace(client, depth, collection.items, {
     storyFields,
     storyLimits,
     defaultNestedLimit,
     nestedCollectionLimit,
+    collectionOfCollectionsIndexes
   }).then(() => collection);
 }
 
-module.exports = {loadNestedCollectionData};
+module.exports = { loadNestedCollectionData };
--- collection-loader.spec.js
+++ collection-loader.spec.js
@@ -1,51 +1,49 @@
-const {loadNestedCollectionData} = require('./collection-loader');
+const { loadNestedCollectionData } = require("./collection-loader");
 
-describe('Collection Loader', function () {
-  it('should load child collection items with required fields', async () => {
+describe("Collection Loader", function() {
+  it("should load child collection items with required fields", async () => {
     const collection = {
-      "slug": "dumbledore-army",
-      "name": "Dumbledore Army",
+      slug: "dumbledore-army",
+      name: "Dumbledore Army",
       "data-source": "automated",
       id: 147,
-      items: [{
-        "id": 147009,
-        "type": "collection",
-        "name": "Potter More",
-        "slug": "potter-more",
-      }],
+      items: [
+        {
+          id: 147009,
+          type: "collection",
+          name: "Potter More",
+          slug: "potter-more"
+        }
+      ],
       "collection-cache-keys": ["c/1088/147"]
     };
     let client = {
       getInBulk: jest.fn().mockResolvedValue({
-        "results": {
+        results: {
           "potter-more-0": {
             "updated-at": 1619675928521,
-            "collection-cache-keys": [
-              "c/1088/147009",
-              "e/1088/195845",
-              "e/1088/198144"
-            ],
-            "slug": "potter-more",
-            "name": "potter more",
+            "collection-cache-keys": ["c/1088/147009", "e/1088/195845", "e/1088/198144"],
+            slug: "potter-more",
+            name: "potter more",
             "data-source": "automated",
-            "automated": true,
-            "template": "default",
-            "rules": {
-              "fields": "author-id,content-type,q,message,story-template,updated-at,id",
+            automated: true,
+            template: "default",
+            rules: {
+              fields: "author-id,content-type,q,message,story-template,updated-at,id",
               "content-type": "story",
-              "sort": "updated-at",
+              sort: "updated-at",
               "collection-id": 147009,
-              "entity-id": "195845,198144",
+              "entity-id": "195845,198144"
             },
-            "summary": "boy who lived",
-            "id": 147009,
-            "items": [
+            summary: "boy who lived",
+            id: 147009,
+            items: [
               {
-                "id": "353b4f32-5fae-4165-9b67-50e0c73902ee",
-                "type": "story"
+                id: "353b4f32-5fae-4165-9b67-50e0c73902ee",
+                type: "story"
               }
             ],
-            "created-at": 1619029886576,
+            "created-at": 1619029886576
           }
         }
       })
@@ -61,34 +59,36 @@ describe('Collection Loader', function () {
     expect(collectionDetails.items[0].automated).toBe(true);
     expect(collectionDetails.items[0].summary).toBe("boy who lived");
     expect(collectionDetails.items[0].items.length).toBe(1);
-    expect(collectionDetails['collection-cache-keys']).toEqual(["c/1088/147"]);
-    expect(collectionDetails.items[0]['collection-cache-keys']).toEqual([
+    expect(collectionDetails["collection-cache-keys"]).toEqual(["c/1088/147"]);
+    expect(collectionDetails.items[0]["collection-cache-keys"]).toEqual([
       "c/1088/147009",
       "e/1088/195845",
       "e/1088/198144"
     ]);
   });
 
-  it('should load child collection items default values for required fields when api does not have values', async () => {
+  it("should load child collection items default values for required fields when api does not have values", async () => {
     const collection = {
-      items: [{
-        "id": 147009,
-        "type": "collection",
-        "name": "Potter More",
-        "slug": "potter-more",
-      }]
+      items: [
+        {
+          id: 147009,
+          type: "collection",
+          name: "Potter More",
+          slug: "potter-more"
+        }
+      ]
     };
     let client = {
       getInBulk: jest.fn().mockResolvedValue({
-        "results": {
+        results: {
           "potter-more-0": {
             "updated-at": 1619675928521,
-            "slug": "potter-more",
-            "name": "potter more",
+            slug: "potter-more",
+            name: "potter more",
             "data-source": "automated",
-            "template": "default",
-            "id": 147009,
-            "created-at": 1619029886576,
+            template: "default",
+            id: 147009,
+            "created-at": 1619029886576
           }
         }
       })
@@ -99,11 +99,67 @@ describe('Collection Loader', function () {
       defaultNestedLimit: 1,
       storyLimits: {}
     });
-
     expect(collectionDetails.items.length).toBe(1);
     expect(collectionDetails.items[0].automated).not.toBeDefined();
     expect(collectionDetails.items[0].summary).toEqual("");
-    expect(collectionDetails.items[0]['collection-cache-keys']).toEqual([]);
+    expect(collectionDetails.items[0]["collection-cache-keys"]).toEqual([]);
     expect(collectionDetails.items[0].items.length).toBe(0);
+  });
+
+  it("should load specific child collection items with collectionOfCollectionsIndexes is passed when depth is 1", async () => {
+    const collection = {
+      slug: "dumbledore-army",
+      name: "Dumbledore Army",
+      "data-source": "automated",
+      id: 147,
+      items: [
+        {
+          id: 147009,
+          type: "collection",
+          name: "Potter More",
+          slug: "potter-more"
+        }
+      ],
+      "collection-cache-keys": ["c/1088/147"]
+    };
+    let client = {
+      getInBulk: jest.fn().mockResolvedValue({
+        results: {
+          "potter-more-0": {
+            "updated-at": 1619675928521,
+            "collection-cache-keys": ["c/1088/147009", "e/1088/195845", "e/1088/198144"],
+            slug: "potter-more",
+            name: "potter more",
+            "data-source": "automated",
+            automated: true,
+            template: "default",
+            rules: {
+              fields: "author-id,content-type,q,message,story-template,updated-at,id",
+              "content-type": "story",
+              sort: "updated-at",
+              "collection-id": 147009,
+              "entity-id": "195845,198144"
+            },
+            summary: "boy who lived",
+            id: 147009,
+            items: [
+              {
+                id: "353b4f32-5fae-4165-9b67-50e0c73902ee",
+                type: "story"
+              }
+            ],
+            "created-at": 1619029886576
+          }
+        }
+      })
+    };
+
+    let collectionDetails = await loadNestedCollectionData(client, collection, {
+      depth: 1,
+      defaultNestedLimit: 1,
+      storyLimits: {},
+      collectionOfCollectionsIndexes: [0]
+    });
+    expect(collectionDetails.items[0].items.length).toBe(1);
   });
 });
