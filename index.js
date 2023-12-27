03a9e76bd9d4accbc38ae9a80d3bbb568b37ef11
phaneendra <58028287+cricket-lover@users.noreply.github.com>
2023-12-27 14:56:07 +0530

    refactor: change variable name in getStoryByExternalId

--- index.js
+++ index.js
@@ -126,12 +126,12 @@ class Story extends BaseAPI {
     return client.getStoryBySlug(slug, params).then(response => this.build(response["story"]));
   }
 
-  static getStoryByExternalId(client, slug) {
-    if (!slug) {
+  static getStoryByExternalId(client, externalId) {
+    if (!externalId) {
       return Promise.resolve(null);
     }
 
-    return client.getStoryByExternalId(slug).then(response => this.build(response["story"]));
+    return client.getStoryByExternalId(externalId).then(response => this.build(response["story"]));
   }
 
   /**
@@ -914,8 +914,8 @@ class Client {
     }).catch(e => catch404(e, {}));
   }
 
-  getStoryByExternalId(slug) {
-    return this.request(`/api/v1/story-by-external-id/${slug}`).catch(e => catch404(e, {}));
+  getStoryByExternalId(externalId) {
+    return this.request(`/api/v1/story-by-external-id/${externalId}`).catch(e => catch404(e, {}));
   }
 
   getStoryById(id) {
